8f9582d1b7eb8ca44efdeaed0f86aad1b2b15295
judthdudeee13 <judthdudeee13@gamil.com>
2025-12-31 16:11:19 -0500

    fullscreen, map loading, and map moving

---
+++ backgrounds.py
@@ -0,0 +1,23 @@
+import pyscroll, pytmx, pygame
+
+class background:
+    def __init__(self, window, background_file, focus, aspect_ratio, width, height):
+        self.aspect_ratio = aspect_ratio
+        self.window = window
+        self.width = width
+        self.height = height
+        self.tmx_data = pytmx.load_pygame(background_file)
+        self.map_data = pyscroll.data.TiledMapData(self.tmx_data)
+        self.focus = focus
+        self.render_surface = pygame.Surface((width, height)).convert_alpha()
+        self.background = pyscroll.BufferedRenderer(self.map_data, (self.width, self.height),clamp_camera=True)
+        self.background_group = pyscroll.PyscrollGroup(map_layer = self.background)
+        self.background_group.add(focus)
+        
+
+    def load(self):
+        cam_x, cam_y = self.focus.rect.center
+        self.background_group.center((int(cam_x), int(cam_y)))
+        self.background_group.draw(self.render_surface)
+        self.scaled_surface = pygame.transform.scale(self.render_surface, (int(self.aspect_ratio[0]*self.width), int(self.aspect_ratio[1]*self.height)))
+        self.window.blit(self.scaled_surface, (0,0))
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import pygame, sys, random, time, pickle, pyscroll,
-
-keys = pygame.key.get_pressed()
-mouse_keys = pygame.mouse.get_pressed()
+#import used modules
+import pygame, sys, random, time, pickle, pyscroll
+import backgrounds
 
 #setup pygame
 pygame.init()
@@ -9,12 +8,21 @@ pygame.mixer.init()
 clock = pygame.time.Clock()
 pygame.mouse.set_visible(False)
 
-#set width and height
-WIDTH  = 16
-HEIGHT = 9
-
 #set up window  
-window = pygame.display.set_mode((WIDTH, HEIGHT), 0, 32)
+window = pygame.display.set_mode((0, 0), pygame.FULLSCREEN, 32)
+
+#screen width and height
+SCREEN_HEIGHT = window.get_height()
+SCREEN_WIDTH = window.get_width()
+
+#set width and height
+WIDTH  = 512
+HEIGHT = 288
+
+#aspect ratio
+WIDTH_RATIO = SCREEN_WIDTH/WIDTH
+HEIGHT_RATIO = SCREEN_HEIGHT/HEIGHT
+
 pygame.display.set_caption('Galaxly of the gods')
 logo = pygame.image.load("game_logo.png")
 pygame.display.set_icon(logo)
@@ -24,13 +32,41 @@ basic = pygame.font.SysFont(None, 75)
 datas = pygame.font.SysFont(None, 25)
 important = pygame.font.SysFont(None, 90)
 
-while True:
+
+# Player
+player = pygame.sprite.Sprite()
+player.image = pygame.Surface((16, 16))
+player.image.fill((255, 0, 0))
+player.rect = player.image.get_rect(topleft = (0, 0))
+
+#backgrounds
+main_world = backgrounds.background(window, "tilesets_tiled/test_world.tmx", player, (WIDTH_RATIO, HEIGHT_RATIO), WIDTH, HEIGHT)
+
+run = True
+while run:
+    #recive mouse and keyboard inputs
+    keys = pygame.key.get_pressed()
+    mouse_keys = pygame.mouse.get_pressed()
+
+    #temporary player
+    if keys[pygame.K_w]:
+        player.rect.y -= 2
+    if keys[pygame.K_s]:
+        player.rect.y +=2
+    if keys[pygame.K_a]:
+        player.rect.x -= 2
+    if keys[pygame.K_d]:
+        player.rect.x += 2
+
+    #load main world map
+    main_world.load()
 
     #make sure window can close
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
+            run = False
+    if keys[pygame.K_ESCAPE]:
+        run = False
 
     #update screen
     pygame.display.update()
@@ -38,4 +74,6 @@ while True:
     #frame rate 60
     clock.tick(60)
 
+pygame.quit()
+sys.exit()
     
