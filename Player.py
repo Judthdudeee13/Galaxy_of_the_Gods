34484c953301cede437b2fb007d01dc41f56f026
judthdudeee13 <judthdudeee13@gamil.com>
2026-01-07 19:59:20 -0500

    added player to litemap

--- Player.py
+++ Player.py
@@ -6,20 +6,20 @@ class Player:
     def update_info(self, file):
         self.file = pytmx.load_pygame(file)
         self.image = self.file.get_layer_by_name("Player")
-        self.rect = self
+        self.rect = None
 
-player = pygame.sprite.Sprite()
-player.image = pygame.Surface((16, 16))
-player.image.fill((255, 0, 0))
-player.rect = player.image.get_rect(topleft = (0, 0))
+# player = pygame.sprite.Sprite()
+# player.image = pygame.Surface((16, 16))
+# player.image.fill((255, 0, 0))
+# player.rect = player.image.get_rect(topleft = (0, 0))
 
 
-#temporary player
-    if keys[pygame.K_w]:
-        player.rect.y -= 2
-    if keys[pygame.K_s]:
-        player.rect.y +=2
-    if keys[pygame.K_a]:
-        player.rect.x -= 2
-    if keys[pygame.K_d]:
-        player.rect.x += 2
+# #temporary player
+#     if keys[pygame.K_w]:
+#         player.rect.y -= 2
+#     if keys[pygame.K_s]:
+#         player.rect.y +=2
+#     if keys[pygame.K_a]:
+#         player.rect.x -= 2
+#     if keys[pygame.K_d]:
+#         player.rect.x += 2
