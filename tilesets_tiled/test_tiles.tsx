88a1f8cc0e0073c9fe26318029c64972ef7eb24b
Judthdudeee13 <Judthdudeee13@gmail.com>
2026-01-08 10:12:47 -0500

    updated tile map

--- tilesets_tiled/test_tiles.tsx
+++ tilesets_tiled/test_tiles.tsx
@@ -1,7 +1,10 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="test_tiles" tilewidth="32" tileheight="32" tilecount="1" columns="0">
+<tileset version="1.10" tiledversion="1.11.2" name="test_tiles" tilewidth="32" tileheight="32" tilecount="2" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image source="pixil-frame-0 (15).png" width="32" height="32"/>
+ </tile>
+ <tile id="2">
+  <image source="pixil-frame-0 (1).png" width="16" height="16"/>
  </tile>
 </tileset>
