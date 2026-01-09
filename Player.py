52c1b95e6bd5687358726038a48d45a4ea043dc4
judthdudeee13 <judthdudeee13@gamil.com>
2026-01-08 21:16:36 -0500

    idk anymore

--- Player.py
+++ Player.py
@@ -1,17 +1,34 @@
-import pygame, pytmx
+import pygame
+import pytmx
+import os
 
 class Player:
-    def __init__(self, file):
-        self.update_info(file)
-    def update_info(self, file):
-        self.file = pytmx.load_pygame(file)
-        self.layer = self.file.get_layer_by_name("Player")
-        self.obj = next(iter(self.layer))
-        self.image_path = self.obj.properties.get("image", None)
-        self.image = pygame.image.load(self.image_path)
-        self.rect = self.image.get_rect(topleft = (int(self.obj.x), int(self.obj.y)))
+    def __init__(self, tmx_file):
+        self.update_info(tmx_file)
 
-    
+    def update_info(self, tmx_file):
+        self.file = pytmx.load_pygame(tmx_file)
+        self.layer = self.file.get_layer_by_name("Player")
+        self.obj = list(self.layer)[0]  # first player object
+
+        # Coordinates
+        self.x = int(self.obj.x)
+        self.y = int(self.obj.y)
+
+        # Get image property
+        image_file = self.obj.properties.get("image")
+        if not image_file:
+            raise ValueError("Player object in TMX is missing 'image' property")
+
+        # Join path relative to TMX folder
+        tmx_dir = os.path.dirname(tmx_file)
+        self.image_path = os.path.join(tmx_dir, image_file)
+
+        if not os.path.isfile(self.image_path):
+            raise FileNotFoundError(f"Cannot find player image: {self.image_path}")
+
+        self.image = pygame.image.load(self.image_path).convert_alpha()
+        self.rect = self.image.get_rect(topleft=(self.x, self.y))
 
 # player = pygame.sprite.Sprite()
 # player.image = pygame.Surface((16, 16))
@@ -28,3 +45,4 @@ class Player:
 #         player.rect.x -= 2
 #     if keys[pygame.K_d]:
 #         player.rect.x += 2
+        
--- tilesets_tiled/test_tiles.tsx
+++ tilesets_tiled/test_tiles.tsx
@@ -1,10 +1,10 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="test_tiles" tilewidth="32" tileheight="32" tilecount="2" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
- <tile id="0">
-  <image source="pixil-frame-0 (15).png" width="32" height="32"/>
+ <tile id="3">
+  <image source="place_holder_background.png" width="32" height="32"/>
  </tile>
- <tile id="2">
-  <image source="pixil-frame-0 (1).png" width="16" height="16"/>
+ <tile id="4">
+  <image source="Place_holder_player.png" width="16" height="16"/>
  </tile>
 </tileset>
