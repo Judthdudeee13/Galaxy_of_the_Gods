fbb3820f3791e5dd0c2f5489256e70bc0ce24a58
judthdudeee13 <judthdudeee13@gamil.com>
2026-01-09 20:00:31 -0500

    Comments on everything

--- Player.py
+++ Player.py
@@ -1,31 +1,38 @@
 import pygame
 import pytmx
 
+#call pygame.sprite.sprite otherwise pyscroll can't use plaer for focus
 class Player(pygame.sprite.Sprite):
     def __init__(self, tmx_file):
+        #needed for sprites
         super().__init__()
         self.load_from_tmx(tmx_file)
+        self.move_speed = 2
 
     def load_from_tmx(self, tmx_file):
         self.tmx = pytmx.load_pygame(tmx_file)
+        #pull player layer
         layer = self.tmx.get_layer_by_name("Player")
+        #pull player obj
         obj = list(layer)[0]
+        #get x and y coords
         self.x = obj.x
         self.y = obj.y
+        #pull image
         self.image = self.tmx.get_tile_image_by_gid(obj.gid)
-        self.rect = self.image.get_rect(
-            topleft=(int(self.x), int(self.y)))
+        self.rect = self.image.get_rect(topleft=(int(self.x), int(self.y)))
 
-        
+    #movement of the player
     def move(self):
+        #takes current key inuts
         keys = pygame.key.get_pressed()
         if keys[pygame.K_w]:
-            self.y -= 2
+            self.y -= self.move_speed
         if keys[pygame.K_s]:
-            self.y +=2
+            self.y += self.move_speed
         if keys[pygame.K_a]:
-            self.x -= 2
+            self.x -= self.move_speed
         if keys[pygame.K_d]:
-            self.x += 2
-
+            self.x += self.move_speed
+        #repotions rect of player
         self.rect.topleft = ((self.x, self.y))
