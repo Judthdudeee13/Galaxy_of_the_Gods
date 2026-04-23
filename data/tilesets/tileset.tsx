155f46ae4bff0d61e88a227df0acc687f3124bbf
judthdudeee13 <judthdudeee13@gamil.com>
2026-04-22 22:47:26 -0400

    save

--- data/tilesets/tileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="tileset" tilewidth="32" tileheight="32" tilecount="484" columns="44">
- <image source="../graphics/tilesets/tile_set.png" width="1408" height="352"/>
-</tileset>
