4ab05352c9ff7ce6ba97b2739ba60b35df2b8355
judthdudeee13 <judthdudeee13@gamil.com>
2026-04-24 08:46:31 -0400

    Revert "new tile sets need fulll object tilesets next"
    This reverts commit 7a74e842f55570c08268764aa660de563269549c.

--- data/tilesets/grass and small plants.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="grass/small plants" tilewidth="32" tileheight="32" tilecount="512" columns="32">
- <image source="../graphics/tilesets/Grass 2 layer.png" width="1024" height="512"/>
-</tileset>
