a8dc55434b65c4298440f4c683b05fc318dbe4f9
judthdudeee13 <judthdudeee13@gamil.com>
2026-04-24 17:17:19 -0400

    save

--- data/tilesets/PlantShadow.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="PlantShadow" tilewidth="32" tileheight="32" tilecount="256" columns="16">
- <image source="../graphics/tilesets/PlantShadow.png" width="512" height="512"/>
-</tileset>
--- data/tilesets/Walls.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="Walls" tilewidth="32" tileheight="32" tilecount="2048" columns="64">
- <image source="../graphics/tilesets/Wall.png" width="2048" height="1024"/>
-</tileset>
