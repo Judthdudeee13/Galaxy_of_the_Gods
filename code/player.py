6509104d97bb02e96d0bd657a1ce964e46f7a884
Judthdudeee13 <judthdudeee13@gmail.com>
2026-04-24 14:48:47 +0000

    save

--- code/player.py
+++ code/player.py
@@ -1,56 +1,58 @@
 from settings import *
 from sprites import *
 
-#player sprite
+
+# player sprite
 class Player(MiltiDirectionalSprite):
     def __init__(self, pos, folders, groups):
         animation_speed = 5.88
         super().__init__(pos, folders, groups, animation_speed)
-        #movment
-        self.speed = 100 * SCALE
+        # movment
+        self.speed = 80 * SCALE
         self.direction = pygame.Vector2(0, 0)
-        self.facing = 'down'
+        self.facing = "down"
 
     def input(self):
         keys = pygame.key.get_pressed()
         self.direction.x = int(keys[pygame.K_d]) - int(keys[pygame.K_a])
         self.direction.y = int(keys[pygame.K_s]) - int(keys[pygame.K_w])
-        self.direction = self.direction.normalize() if self.direction else self.direction
+        self.direction = (
+            self.direction.normalize() if self.direction else self.direction
+        )
 
     def move(self, dt):
-        self.rect.x += self.direction.x * self.speed * dt 
-        self.check_collision('horizontal')
-        self.rect.y += self.direction.y * self.speed * dt 
-        self.check_collision('vertical')
+        self.rect.x += self.direction.x * self.speed * dt
+        self.check_collision("horizontal")
+        self.rect.y += self.direction.y * self.speed * dt
+        self.check_collision("vertical")
 
     def direction_check(self):
         if self.direction:
             if self.direction.x:
                 if self.direction.x > 0:
-                    self.facing = 'right'
+                    self.facing = "right"
                 else:
-                    self.facing = 'left'
+                    self.facing = "left"
             elif self.direction.y:
-                if self.direction.y > 0 :
-                    self.facing = 'down'
+                if self.direction.y > 0:
+                    self.facing = "down"
                 else:
-                    self.facing = 'up'
+                    self.facing = "up"
         else:
-            if self.facing == 'right':
-                self.facing = 'idle_right'
-            elif self.facing == 'left':
-                self.facing = 'idle_left'
-            elif self.facing == 'up':
-                self.facing = 'idle_up'
-            elif self.facing == 'down':
-                self.facing = 'idle_down'
-        
+            if self.facing == "right":
+                self.facing = "idle_right"
+            elif self.facing == "left":
+                self.facing = "idle_left"
+            elif self.facing == "up":
+                self.facing = "idle_up"
+            elif self.facing == "down":
+                self.facing = "idle_down"
+
         self.update_direction(self.facing)
 
     def draw(self, dt):
         self.direction_check()
         self.animate(dt)
-        
 
     def check_collision(self, direction):
         pass
