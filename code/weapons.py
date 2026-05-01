807b7424f1fa13a076fef256e89c29443dd2b76f
Judthdudeee13 <judthdudeee13@gmail.com>
2026-05-01 08:37:30 -0400

    sae

--- code/weapons.py
+++ code/weapons.py
@@ -1,5 +1,9 @@
 from settings import *
 
 class Weapon:
-    def __init__(self, damage, type, range, damage_type = None):
-        pass
+    def __init__(self, damage, range, damage_type = None):
+        self.damage = damage
+        self.range = range
+        self.damage_type = damage_type
+
+        
